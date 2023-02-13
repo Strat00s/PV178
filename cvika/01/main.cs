3b73d940f3e2e36a191153e4b21bf2cb10fca43e
Lukáš Baštýř <l.bastyr@seznam.cz>
2023-02-13 19:30:16 +0100

    nothing

--- cvika/01/main.cs
+++ cvika/01/main.cs
@@ -1,22 +1,28 @@
 //exponentiation
 using System;
 
-class Expo {
+class expo {
     static void Main(string[] args) {
         if (args.Length > 1) {
             Console.WriteLine(Math.Pow(Int32.Parse(args[0]), Int32.Parse(args[1])));
             return;
         }
+    
         string line = Console.ReadLine();
         var splits = line.Split(' ');
-        if (splits.Length < 1)
+    
+        if (splits.Length < 1) {
+            Console.WriteLine("Not enough arguments! Exiting...");
             return;
+        }
+    
         if (splits.Length > 1) {
             Console.WriteLine(Math.Pow(Int32.Parse(splits[0]), Int32.Parse(splits[1])));
             return;
         }
+    
         int base_num = Int32.Parse(splits[0]);
-        int exp = Int32.Parse(Console.ReadLine());
+        int exp = Int32.Parse(Console.ReadLine().Split(' ')[0]);
         Console.WriteLine(Math.Pow(base_num, exp));
     }
 }
